334805bc59ba188bd79371865c822a042665a7b8
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-01-05 19:09:32 +0100

    Implement main loop with sprite groups in Pygame

--- main.py
+++ main.py
@@ -11,36 +11,40 @@ def main():
     # print('Screen height:', SCREEN_HEIGHT)
     
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(
         x = SCREEN_WIDTH / 2,
         y = SCREEN_HEIGHT / 2,
     )
-   
 
+    dt = 0
+   
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
+            
+        for update in updatable:
+            update.update(dt)
 
-        screen.fill(pygame.Color('black'))      
-        player.draw(screen)     
+        screen.fill(pygame.Color('black'))
+
+        for draw in drawable:
+            draw.draw(screen)
+
         pygame.display.flip()
-        
-        # rotate player to the left and right
-        player.update(dt)
-        
         
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
     
-
 if __name__ == "__main__":
     main()
     
